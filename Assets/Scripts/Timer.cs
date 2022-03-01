a12bf400cda5e101d4ccd5b448a82b1b30eae8ce
Lev <lev.shchinov@ya.ru>
2022-03-01 17:38:25 +0300

    LefVse

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Timer : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+        private float i;
+
+
+        private void Awake()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(StartTimerRoutine(DateTime.Now.AddHours(1)));
+        }
+
+        private IEnumerator StartTimerRoutine(DateTime dateTime)
+        {
+            for (i = dateTime.Second; i > 0; i-=1)
+            {
+                yield return new WaitForSeconds(1);
+                _text.SetText(i.ToString());
+            }
+        }
+
+        public void RemoveTime(float time)
+        {
+            i -= time;
+        }
+    }
+}
